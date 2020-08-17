9a1df68fe94ccc098d2c792536d863f999e71bb1
Priyank Mithaiwala <priyankmithaiwala@gmail.com>
2020-08-17 14:07:43 +0530

    Updates

---
+++ precache-manifest.79372c492c64d3be82ce68ec6ec3ac3f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4877ce9783af2f71c1da5df2507f256",
+    "url": "/Pm_Developer/index.html"
+  },
+  {
+    "revision": "bfcfd8e321ba1c6e63bd",
+    "url": "/Pm_Developer/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "bd19f4244e5648bfe0a9",
+    "url": "/Pm_Developer/static/css/main.b12c51a2.chunk.css"
+  },
+  {
+    "revision": "bfcfd8e321ba1c6e63bd",
+    "url": "/Pm_Developer/static/js/2.4f59ae5c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Pm_Developer/static/js/2.4f59ae5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd19f4244e5648bfe0a9",
+    "url": "/Pm_Developer/static/js/main.124d4805.chunk.js"
+  },
+  {
+    "revision": "955310c9514ef26139c2",
+    "url": "/Pm_Developer/static/js/runtime-main.3d1222f5.js"
+  },
+  {
+    "revision": "59a17b90060ae8efe91ddd9a1cf07917",
+    "url": "/Pm_Developer/static/media/Startup_SVG.59a17b90.svg"
+  },
+  {
+    "revision": "bd789dc8f45e372a7d169a97bc6a3087",
+    "url": "/Pm_Developer/static/media/android.bd789dc8.jpg"
+  },
+  {
+    "revision": "c382b39fe94f323cbed59fdd9b17d7b3",
+    "url": "/Pm_Developer/static/media/app.c382b39f.jpg"
+  },
+  {
+    "revision": "b072d5a69a7d76711ef057c48c36a050",
+    "url": "/Pm_Developer/static/media/bg_3.b072d5a6.jpg"
+  },
+  {
+    "revision": "614fd2bd044f8ab88e99bb20febc0669",
+    "url": "/Pm_Developer/static/media/digital.614fd2bd.jpg"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Pm_Developer/static/media/homeimg2.e981619f.svg"
+  },
+  {
+    "revision": "b8b45127b4dac7029a6050a27c50a51f",
+    "url": "/Pm_Developer/static/media/marketing.b8b45127.jpg"
+  },
+  {
+    "revision": "7f8fcac6dcce2cadf6bb49a29898d924",
+    "url": "/Pm_Developer/static/media/software.7f8fcac6.jpg"
+  }
+]);
